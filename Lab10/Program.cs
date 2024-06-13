9906571c688ab3093296faabe16eef8500d37932
Tyrtyshnyi Yaroslav <yarikbestway@gmail.com>
2024-06-13 16:20:19 +0300

    Добавьте файлы проекта.

---
+++ Lab10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 3; 
+            int[][] matrix = new int[N][];
+            matrix[0] = new int[] { 8, 2, 5 };
+            matrix[1] = new int[] { 1, 1, 6 };
+            matrix[2] = new int[] { 9, 8, 7 };
+
+            int[] diagonal = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                diagonal[i] = matrix[i][i];
+            }
+
+            Array.Sort(diagonal);
+
+            Console.WriteLine("Відсортована головна діагональ:");
+            foreach (int num in diagonal)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
