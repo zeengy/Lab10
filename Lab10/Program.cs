c8fcd6faa3571fb46ad7e0b60ddad8824d5826f3
YaroslavTyrtyshnyi <148007608+zeengy@users.noreply.github.com>
2024-06-19 16:12:25 +0300

    Update Program.cs

--- Lab10/Program.cs
+++ Lab10/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +8,21 @@ namespace Lab10
 {
     internal class Program
     {
-        static void Main(string[] args)
+         static unsafe void Main(string[] args)
         {
-            int N = 3; 
-            int[][] matrix = new int[N][];
-            matrix[0] = new int[] { 8, 2, 5 };
-            matrix[1] = new int[] { 1, 1, 6 };
-            matrix[2] = new int[] { 9, 8, 7 };
+            int N = 3;
+
+            int*[] matrix = new int*[N];
+
+            matrix[0] = stackalloc int[N] { 8, 2, 5 };
+            matrix[1] = stackalloc int[N] { 1, 1, 6 };
+            matrix[2] = stackalloc int[N] { 9, 8, 7 };
 
             int[] diagonal = new int[N];
 
             for (int i = 0; i < N; i++)
             {
-                diagonal[i] = matrix[i][i];
+                diagonal[i] = *(matrix[i] + i);
             }
 
             Array.Sort(diagonal);
@@ -30,6 +32,18 @@ namespace Lab10
             {
                 Console.Write(num + " ");
             }
+            Console.WriteLine();
+
+            Console.WriteLine("Матриця:");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(*(matrix[i] + j) + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
+    
 }
